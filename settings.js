cde1ff76f9b64af0780629a242ec60d69c4653fe
Josrenta <109117260+Josrenta@users.noreply.github.com>
2022-07-12 11:24:55 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['085762671932'] //ur owner number
+global.ownername = "Josrenta" //ur owner name
+global.ytname = "YT: Josrenta - GTPS" //ur yt chanel name
+global.socialm = "GitHub: Josrenta" //ur github or insta name
+global.location = "Indonesia, Sumut, Medan" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.owner = ['085762671932']
+global.ownertag = '085762671932' //ur tag number
+global.botname = 'Jos bot' //ur bot name
+global.linkz = "https://chat.whatsapp.com/BAHYqkPsKcFFeQETTRQunl" //your theme url which will be displayed on whatsapp
+global.websitex = "https://chat.whatsapp.com/BAHYqkPsKcFFeQETTRQunl" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.themeemoji = "🗿" //ur theme emoji
+global.packname = "Jos Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.author = "Josrenta" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
